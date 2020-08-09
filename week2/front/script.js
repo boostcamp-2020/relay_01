8517b505329a4c35e496a1fd9b935e8be9fdf21a
enhakkore <ahn.ephraim@gmail.com>
2020-08-09 16:35:40 +0900

    [week2] script.js 수정

--- week2/front/script.js
+++ week2/front/script.js
@@ -7,6 +7,7 @@ function init() {
 
   let main = document.getElementById("main");
   let api = new API();
+  let api2 = new ShutterstockAPI();
 
   button.addEventListener("click", async (e) => {
     inputValue = input.value;
@@ -14,19 +15,29 @@ function init() {
     try {
       ret = await api.getAdam(inputValue);
     } catch (e) {
-      alert("Data-fetching-Error!");
+      alert("Adam-Data-fetching-Error!");
     }
 
-    let mood = moodParser(ret.data); // 입력한 문장을 기반으로 받아온 JSON Object에서 감정만 받아온다.
+    let moodFromAdam = moodParser(ret.data); // 입력한 문장을 기반으로 받아온 JSON Object에서 감정만 받아온다.
+    let mood = moods[moodFromAdam];
+
+    let audioResult;
+    try{
+      audioResult = await api2.getAudio(mood);
+    }catch(e){
+      alert("Audio-Data-fetching-Error!");
+    }
+
+    audioResult = audioResult.data.data
 
     await removeMusic();
 
-    musicJson
-      .filter((obj) => obj.mood === mood)
-      .forEach((obj) => {
+    audioResult
+      .forEach(obj => {
         document
           .getElementById("track-list")
-          .appendChild(getMusicJsonNode(obj));
+          .appendChild(getMusicJsonNode(obj, mood))
+
       });
   });
 }
@@ -43,19 +54,20 @@ const removeMusic = () => {
   mainNode.appendChild(newTrackListNode);
 };
 
-const getMusicJsonNode = (obj) => {
+const getMusicJsonNode = (obj, mood) => {
   /*
   아래와 같은 구조의 DOM 생성
 
   <div class="track">
     <img class="thumbnail" src="./assets/thumbnail.jpg" />
-        <div class="track-info-wrapper">
-            <div class="name">제목 : Kill this Love!</div>
-            <div class="artist">아티스트 : 블랙핑크</div>
-            <div class="mood">분위기 : 격앙</div>
-          </div>
-   </div>
-   
+    <div class="track-info-wrapper">
+        <div class="name">Title : With Hope</div>
+        <div class="artist">Description : Warm and flowing, with production...</div>
+        <div class="mood">Mood : Happy...</div>
+        <audio class="preview_mp3" src="http://~" constrols=true />
+    </div>
+  </div>
+
   */
 
   // 노드들 attribute 설정
@@ -64,32 +76,38 @@ const getMusicJsonNode = (obj) => {
 
   let thumbNailNode = document.createElement("img");
   thumbNailNode.setAttribute("class", "thumbnail");
-  thumbNailNode.setAttribute("src", obj.artwork);
+  thumbNailNode.setAttribute("src", obj.assets.waveform.url);
 
   let trackInfoWrapperNode = document.createElement("div");
   trackInfoWrapperNode.setAttribute("class", "track-info-wrapper");
 
-  let nameNode = document.createElement("div");
-  nameNode.setAttribute("class", "name");
-  let nameText = document.createTextNode(`제목 : ${obj.title}`);
-  nameNode.appendChild(nameText);
+  let titleNode = document.createElement("div");
+  titleNode.setAttribute("class", "title");
+  let titleText = document.createTextNode(`Title : ${obj.title}`);
+  titleNode.appendChild(titleText);
 
-  let artistNode = document.createElement("div");
-  artistNode.setAttribute("class", "artist");
-  let artistText = document.createTextNode(`아티스트 : ${obj.artist}`);
-  artistNode.appendChild(artistText);
+  let desNode = document.createElement("div");
+  desNode.setAttribute("class", "description");
+  let desText = document.createTextNode(`Description : ${obj.description}`);
+  desNode.appendChild(desText);
 
   let moodNode = document.createElement("div");
   moodNode.setAttribute("class", "mood");
-  let moodText = document.createTextNode(`분위기 : ${obj.mood}`);
+  let moodText = document.createTextNode(`Mood : ${mood}`);
   moodNode.appendChild(moodText);
+
+  let preMp3Node = document.createElement("audio");
+  preMp3Node.setAttribute("class", "preview_mp3")
+  preMp3Node.setAttribute("controls", true);
+  preMp3Node.setAttribute("src", obj.assets.preview_mp3.url);
 
   // 노드들 부모, 자식 관계 설정
   trackNode.appendChild(thumbNailNode);
   trackNode.appendChild(trackInfoWrapperNode);
-  trackInfoWrapperNode.appendChild(nameNode);
-  trackInfoWrapperNode.appendChild(artistNode);
+  trackInfoWrapperNode.appendChild(titleNode);
+  trackInfoWrapperNode.appendChild(desNode);
   trackInfoWrapperNode.appendChild(moodNode);
+  trackInfoWrapperNode.appendChild(preMp3Node);
 
   return trackNode;
 };
